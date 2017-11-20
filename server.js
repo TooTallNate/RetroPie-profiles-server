e5dcb2a7c789a25d2dc04a581fe864ccb95ac28a
Nathan Rajlich <nathan@tootallnate.net>
2017-11-19 22:29:21 -0800

    prettier

--- server.js
+++ server.js
@@ -17,18 +17,18 @@ const emitter = new EventEmitter()
 
 let filename = process.sub[1]
 if (!isAbsolute(filename)) {
-  filename = resolve(filename);
+  filename = resolve(filename)
 }
 
-debug('entry point: %o', filename);
-let mod = require(filename);
-if (mod.default) mod = mod.default;
+debug('entry point: %o', filename)
+let mod = require(filename)
+if (mod.default) mod = mod.default
 
 if ('function' !== typeof mod) {
-  throw new TypeError(`A function must be exported. Got "${typeof mod}"`);
+  throw new TypeError(`A function must be exported. Got "${typeof mod}"`)
 }
 
-module.exports = async function (req, res) {
+module.exports = async function(req, res) {
   res.setHeader('Connection', 'close')
 
   const parsed = parse(req.url)
@@ -44,7 +44,7 @@ module.exports = async function (req, res) {
 
     // return a string that bash can eval for env vars
     const env = Object.keys(login)
-      .map((key) => {
+      .map(key => {
         const name = snakeCase(key).toUpperCase()
         const value = JSON.stringify(login[key])
         return `export ${name}=${value}`
@@ -52,14 +52,13 @@ module.exports = async function (req, res) {
       .join('\n')
     debug('returning env to login.sh: %o', env)
     return env
-
   } else {
-    res.once('login', (login) => emitter.emit('login', login))
+    res.once('login', login => emitter.emit('login', login))
     return Promise.resolve(mod(req, res))
   }
 }
 
-function waitEvent (emitter, name) {
+function waitEvent(emitter, name) {
   return new Promise((resolve, reject) => {
     emitter.once(name, resolve)
   })
